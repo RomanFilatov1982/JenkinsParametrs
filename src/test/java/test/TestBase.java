b157827e265d90060e26d15c0104a4e77a63ba9b
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-07-23 19:19:38 +0300

    change Configurations

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -16,12 +16,12 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        //Configuration.browser = "chrome";
-        Configuration.timeout = 10000;
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com/");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "138.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         //Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = System.getProperty("remoteUrl");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
